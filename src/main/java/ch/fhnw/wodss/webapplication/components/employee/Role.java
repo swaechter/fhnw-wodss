837a21aa791c90ac2af10a9a5514ca7e3e61bc34
Simon Wächter <waechter.simon@gmail.com>
2019-03-17 23:47:51 +0100

    Code cleanup

--- src/main/java/ch/fhnw/wodss/webapplication/components/employee/Role.java
+++ src/main/java/ch/fhnw/wodss/webapplication/components/employee/Role.java
@@ -1,27 +1,7 @@
 package ch.fhnw.wodss.webapplication.components.employee;
 
 public enum Role {
-    ADMINISTRATOR,//("ADMINISTRATOR"),
-    PROJECTMANAGER,//("PROJECTMANAGER"),
-    DEVELOPER;//("DEVELOPER");
-
-    /*@JsonCreator
-    public static Role getRoleFromValue(String value) {
-        for (Role role : values()) {
-            if (role.roleName.equals(value)) {// .valueInJson.equals(value)) {
-                return role;
-            }
-        }
-        throw new IllegalArgumentException();
-    }*/
-
-/*    private final String roleName;
-
-    Role(String roleName) {
-        this.roleName = roleName;
-    }
-
-    public String getRoleName() {
-        return roleName;
-    }*/
+    ADMINISTRATOR,
+    PROJECTMANAGER,
+    DEVELOPER;
 }
