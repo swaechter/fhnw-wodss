3f92558d24172dd95801837f702fba6758431c5d
Philipp Lüthi <philipp@luethi.rocks>
2019-04-09 09:52:13 +0200

    Add autocomplete tags
    https://www.chromium.org/developers/design-documents/create-amazing-password-forms

--- src/main/javascript/src/pages/login/login.js
+++ src/main/javascript/src/pages/login/login.js
@@ -49,12 +49,15 @@ export default class LoginPage extends Component {
                                     name="emailAddress"
                                     value={this.state.emailAddress}
                                     onChange={this.handleInputChange}
-                                    placeholder="Enter email" />
+                                    placeholder="Enter email"
+                                    autocomplete="email" />
                                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"
+                                <input type="password" class="form-control" id="exampleInputPassword1" 
+                                    placeholder="Password"
+                                    autocomplete="current-password"
                                     name="rawPassword"
                                     value={this.state.rawPassword}
                                     onChange={this.handleInputChange} />
