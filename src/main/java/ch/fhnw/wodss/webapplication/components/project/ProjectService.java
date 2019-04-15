095470a91e3de3394619877bb23e019b1b531852
Thibault Gagnaux <thibault.gagnaux@students.fhnw.ch>
2019-04-15 16:47:40 +0200

    Deletes Project Interface again, refactores, Unit Test

--- src/main/java/ch/fhnw/wodss/webapplication/components/project/ProjectService.java
+++ src/main/java/ch/fhnw/wodss/webapplication/components/project/ProjectService.java
@@ -1,32 +1,90 @@
 package ch.fhnw.wodss.webapplication.components.project;
 
+import ch.fhnw.wodss.webapplication.components.employee.EmployeeDto;
+import ch.fhnw.wodss.webapplication.components.employee.EmployeeRepository;
+import ch.fhnw.wodss.webapplication.components.employee.Role;
 import ch.fhnw.wodss.webapplication.configuration.AuthenticatedEmployee;
+import ch.fhnw.wodss.webapplication.exceptions.EntityNotFoundException;
+import ch.fhnw.wodss.webapplication.exceptions.InternalException;
+import ch.fhnw.wodss.webapplication.exceptions.NotAuthorizedException;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
-public interface ProjectService {
+@Service
+public class ProjectService {
 
-    ProjectDto createProject(
-        ProjectDto project,
-        AuthenticatedEmployee authenticatedEmployee);
+    private final ProjectRepository projectRepository;
 
-    List<ProjectDto> getProjects(
-        LocalDate fromDate,
-        LocalDate toDate,
-        Long projectManagerId,
-        AuthenticatedEmployee authenticatedEmployee);
+    private final EmployeeRepository employeeRepository;
 
-    ProjectDto getProject(
-        Long id,
-        AuthenticatedEmployee authenticatedEmployee);
+    public ProjectService(ProjectRepository projectRepository, EmployeeRepository employeeRepository) {
+        this.projectRepository = projectRepository;
+        this.employeeRepository = employeeRepository;
+    }
 
-    ProjectDto updateProject(
-        Long id,
-        ProjectDto project,
-        AuthenticatedEmployee authenticatedEmployee);
+    public ProjectDto createProject(ProjectDto project, AuthenticatedEmployee authenticatedEmployee) {
+        if (project == null || authenticatedEmployee == null) {
+            throw new IllegalArgumentException("Project or Employee must not be null");
+        }
 
-    void deleteProject(
-        Long id,
-        AuthenticatedEmployee authenticatedEmployee);
+        if (!authenticatedEmployee.getRole().equals(Role.ADMINISTRATOR)) {
+            throw new NotAuthorizedException("Not authorized to create project");
+        }
+
+        Optional<ProjectDto> createdProject = projectRepository.saveProject(project);
+        if (createdProject.isEmpty()) {
+            throw new InternalException("Unable to create the project");
+        }
+        Optional<EmployeeDto> selectedEmployee = employeeRepository.getEmployeeById(project.getProjectManagerId());
+        if (selectedEmployee.isEmpty()) {
+            throw new EntityNotFoundException("employee", project.getProjectManagerId());
+        }
+
+        return createdProject.get();
+    }
+
+
+    public List<ProjectDto> getProjects(LocalDate fromDate, LocalDate toDate, Long projectManagerId, AuthenticatedEmployee authenticatedEmployee) {
+        return projectRepository.getProjects(fromDate, toDate, projectManagerId);
+    }
+
+    public ProjectDto getProject(Long id, AuthenticatedEmployee authenticatedEmployee) {
+        Optional<ProjectDto> selectedProject = projectRepository.getProjectById(id);
+        if (selectedProject.isEmpty()) {
+            throw new EntityNotFoundException("project", id);
+        }
+
+        return selectedProject.get();
+    }
+
+    public ProjectDto updateProject(Long id, ProjectDto project, AuthenticatedEmployee authenticatedEmployee) {
+        Optional<ProjectDto> selectedProject = projectRepository.getProjectById(id);
+        if (selectedProject.isEmpty()) {
+            throw new EntityNotFoundException("project", id);
+        }
+
+        Optional<EmployeeDto> selectedEmployee = employeeRepository.getEmployeeById(project.getProjectManagerId());
+        if (selectedEmployee.isEmpty()) {
+            throw new EntityNotFoundException("employee", project.getProjectManagerId());
+        }
+
+        Optional<ProjectDto> updatedProject = projectRepository.updateProject(id, project);
+        if (updatedProject.isEmpty()) {
+            throw new InternalException("Unable to update the project");
+        }
+
+        return updatedProject.get();
+    }
+
+    public void deleteProject(Long id, AuthenticatedEmployee authenticatedEmployee) {
+        Optional<ProjectDto> selectedProject = projectRepository.getProjectById(id);
+        if (selectedProject.isEmpty()) {
+            throw new EntityNotFoundException("project", id);
+        }
+
+        projectRepository.deleteProject(id);
+    }
 }
--- src/test/java/ch/fhnw/wodss/webapplication/service/ProjectServiceUnitTest.java
+++ src/test/java/ch/fhnw/wodss/webapplication/service/ProjectServiceUnitTest.java
@@ -5,7 +5,7 @@ import ch.fhnw.wodss.webapplication.components.employee.EmployeeRepository;
 import ch.fhnw.wodss.webapplication.components.employee.Role;
 import ch.fhnw.wodss.webapplication.components.project.ProjectDto;
 import ch.fhnw.wodss.webapplication.components.project.ProjectRepository;
-import ch.fhnw.wodss.webapplication.components.project.ProjectServiceImpl;
+import ch.fhnw.wodss.webapplication.components.project.ProjectService;
 import ch.fhnw.wodss.webapplication.configuration.AuthenticatedEmployee;
 import ch.fhnw.wodss.webapplication.exceptions.EntityNotFoundException;
 import ch.fhnw.wodss.webapplication.exceptions.InternalException;
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
-public class ProjectServiceImplUnitTest {
+public class ProjectServiceUnitTest {
     @Mock
     private ProjectRepository projectRepo;
 
@@ -34,32 +34,28 @@ public class ProjectServiceImplUnitTest {
     @Mock
     private ProjectDto mockProject;
 
-    private ProjectServiceImpl projectService;
+    private ProjectService projectService;
 
     @BeforeEach
-    public void setUp()
-    {
-        projectService = new ProjectServiceImpl(projectRepo, employeeRepo);
+    public void setUp() {
+        projectService = new ProjectService(projectRepo, employeeRepo);
     }
 
     @Test
-    public void whenProjectOrEmployeeIsNull_thenCannotCreateProject()
-    {
+    public void whenProjectOrEmployeeIsNull_thenCannotCreateProject() {
         assertException(IllegalArgumentException.class, null, mockEmployee);
         assertException(IllegalArgumentException.class, mockProject, null);
 
     }
 
     @Test
-    public void whenDeveloper_thenCannotCreateProject()
-    {
+    public void whenDeveloper_thenCannotCreateProject() {
         given(mockEmployee.getRole()).willReturn(Role.DEVELOPER);
         assertException(NotAuthorizedException.class, mockProject, mockEmployee);
     }
 
     @Test
-    public void whenProjectManager_thenCannotCreateProject()
-    {
+    public void whenProjectManager_thenCannotCreateProject() {
         given(mockEmployee.getRole()).willReturn(Role.PROJECTMANAGER);
         assertException(NotAuthorizedException.class, mockProject, mockEmployee);
     }
@@ -67,36 +63,25 @@ public class ProjectServiceImplUnitTest {
     @Nested
     public class whenEmployeeIsAdministrator {
         @BeforeEach
-        public void setUp()
-        {
+        public void setUp() {
             given(mockEmployee.getRole()).willReturn(Role.ADMINISTRATOR);
         }
 
         @Test
-        public void whenProjectCannotBeSavedToDatabase_thenThrowException()
-        {
+        public void whenProjectCannotBeSavedToDatabase_thenThrowException() {
             given(projectRepo.saveProject(mockProject)).willReturn(Optional.empty());
             assertException(InternalException.class, mockProject, mockEmployee);
         }
 
         @Test
-        public void whenEmployeeNotFound_thenThrowException()
-        {
-            given(projectRepo.saveProject(mockProject)).willReturn(Optional.of(
-                mockProject));
-            given(employeeRepo.getEmployeeById(mockProject.getProjectManagerId())).willReturn(
-                Optional.empty());
-            assertException(EntityNotFoundException.class,
-                            mockProject,
-                            mockEmployee);
+        public void whenEmployeeNotFound_thenThrowException() {
+            given(projectRepo.saveProject(mockProject)).willReturn(Optional.of(mockProject));
+            given(employeeRepo.getEmployeeById(mockProject.getProjectManagerId())).willReturn(Optional.empty());
+            assertException(EntityNotFoundException.class, mockProject, mockEmployee);
         }
     }
 
-    private void assertException(
-        Class<? extends Throwable> cl,
-        ProjectDto project,
-        AuthenticatedEmployee employee)
-    {
+    private void assertException(Class<? extends Throwable> cl, ProjectDto project, AuthenticatedEmployee employee) {
         assertThrows(cl, () -> projectService.createProject(project, employee));
     }
 }
