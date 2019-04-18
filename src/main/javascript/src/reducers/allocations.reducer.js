caee8b5df6d7c260639ce2bc16fd41b60b151d5c
Philipp Lüthi <philippluca.luethi@gmail.com>
2019-04-18 18:36:25 +0200

    Convert allocation strings to dates on success

--- src/main/javascript/src/reducers/allocations.reducer.js
+++ src/main/javascript/src/reducers/allocations.reducer.js
@@ -14,7 +14,13 @@ export function allocations(state = initialState(), action) {
 		case FETCH_ALLOCATIONS_BEGIN:
 			return state;
 		case FETCH_ALLOCATIONS_SUCCESS:
-			let joinedArray = [...state, ...action.payload]
+			let newEntries = action.payload.map((item) => {
+				return {
+					...item, 
+					startDate: new Date(item.startDate),
+					endDate: new Date(item.endDate)}
+				})
+			let joinedArray = [...state, ...newEntries]
 			return unique(joinedArray, (item) => item.id)
 		case FETCH_ALLOCATIONS_FAIL:
 			return [];
