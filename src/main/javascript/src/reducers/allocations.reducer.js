a94bb41e2faea5162c75360ed691fcf6d1a9f975
Philipp Lüthi <philippluca.luethi@gmail.com>
2019-04-18 18:05:16 +0200

    Merge Allocations after fetch

--- src/main/javascript/src/reducers/allocations.reducer.js
+++ src/main/javascript/src/reducers/allocations.reducer.js
@@ -3,6 +3,7 @@ import {
 	FETCH_ALLOCATIONS_SUCCESS,
 	FETCH_ALLOCATIONS_FAIL
 } from '../actions';
+import { unique } from '../utils/filters';
 
 const initialState = () => {
 	return [];
@@ -13,7 +14,8 @@ export function allocations(state = initialState(), action) {
 		case FETCH_ALLOCATIONS_BEGIN:
 			return state;
 		case FETCH_ALLOCATIONS_SUCCESS:
-			return action.payload;
+			let joinedArray = [...state, ...action.payload]
+			return unique(joinedArray, (item) => item.id)
 		case FETCH_ALLOCATIONS_FAIL:
 			return [];
 		default:
--- src/main/javascript/src/utils/filters.js
+++ src/main/javascript/src/utils/filters.js
@@ -1,11 +1,12 @@
-const unique = (array = [], getIdentifier) => {
+export const unique = (array, getIdentifier) => {
     let result = []
     let ids = new Set()
-    for(item of array){
+    for(let item of array){
         let id = getIdentifier(item)
         if(!ids.has(id)){
             ids.add(id)
             result.push(item)
         }
     }
+    return result
 }
