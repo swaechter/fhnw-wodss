e63eb7445911893036c1fb869d8ccb1c768604ea
Simon Wächter <waechter.simon@gmail.com>
2019-04-24 09:21:21 +0200

    Various fixes for URL handling + a renamed Preact CLI serve command

--- src/main/javascript/src/pages/app.js
+++ src/main/javascript/src/pages/app.js
@@ -1,11 +1,10 @@
-import { h, Component } from 'preact';
 
-import { createHashHistory } from 'history';
-import { Router, route } from 'preact-router';
-
-//require('Bootstrap')
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
+
+import {createHashHistory} from 'history';
+import {Component} from 'preact';
+import {Router} from 'preact-router';
 import Redirect from '../components/redirect';
 import TodoPage from './todo/todo-page';
 import ProjectPage from './projects/projects';
@@ -15,16 +14,16 @@ import MyAllocationsPage from './my-allocations/my-allocations';
 
 export default class App extends Component {
 
-	render() {
-		return (
-			<AuthLock>
-				<Router history={createHashHistory()}>
-					<TodoPage path='/todo' />
-					<MyAllocationsPage path='/my-allocation' />
-					<ProjectPage path='/project' />
-					<Redirect default to="/project" />
-				</Router>
-			</AuthLock>
-		);
-	}
+    render() {
+        return (
+            <AuthLock>
+                <Router history={createHashHistory()}>
+                    <TodoPage path='/todo'/>
+                    <MyAllocationsPage path='/my-allocation'/>
+                    <ProjectPage path='/project'/>
+                    <Redirect default to="/my-allocation"/>
+                </Router>
+            </AuthLock>
+        );
+    }
 }
--- src/main/javascript/src/services/config.js
+++ src/main/javascript/src/services/config.js
@@ -1,1 +1,1 @@
-export const apiServerUrl = 'https://fhnw-wodss-philipp.herokuapp.com/'
+export const apiServerUrl = 'https://fhnw-wodss-philipp.herokuapp.com/';
